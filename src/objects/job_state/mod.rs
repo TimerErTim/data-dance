f9c70e797f5a5b430fcedd6bf05a083a4cd9e8f3
TimerErTim <timerertim@gmail.com>
2025-11-18 13:15:50 +0100

    build: add openapi spec generation tasks

--- src/objects/job_state/mod.rs
+++ src/objects/job_state/mod.rs
@@ -7,10 +7,13 @@ pub use incremental_backup::*;
 
 #[derive(Clone, Debug, Serialize, Deserialize, Object)]
 pub struct JobStates {
+    /// Contains the state of the restore job if it is running.
     pub restore: Option<RestoreJobState>,
+    /// Contains the state of the backup job if it is running.
     pub backup: Option<BackupJobState>,
 }
 
+/// This is a BackupJobState union type. It is used to represent the state of a backup job.
 #[derive(Clone, Debug, Serialize, Deserialize, Union)]
 #[oai(discriminator_name = "type")]
 pub enum BackupJobState {
